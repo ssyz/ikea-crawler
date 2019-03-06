3a485f503850e11989fb0a76fd102e804dfb5310
Jay Syz <jaysyz@annuitas.com>
2019-03-06 15:05:59 -0500

    add prods, fix package data

--- bs4_test.py
+++ bs4_test.py
@@ -10,7 +10,7 @@ import requests
 """ retrieve product ids from department page """
 def get_ids(s):
     prods = []
-    for link in soup.find_all('a'):
+    for link in s.find_all('a'):
         prod = '/us/en/catalog/products/'
 
         l = link.get('href')
@@ -26,48 +26,50 @@ def get_ids(s):
 
 """ retrieve product information from product page """
 def get_prod_info(s):
+    print("-------- PRODUCT EXTRACTION LOGS --------")
+
     info = []
 
     try:
         product_name = s.h1.span.contents[0].strip()
         info.append(product_name)
     except Exception as e:
-        print("information extraction error: " + str(e))
+        print("Product Name extraction error: " + str(e))
         info.append("na")
 
     try:
         article_number = s.find("div", {"id": "itemNumber"}).contents[0]
         info.append(article_number)
     except Exception as e:
-        print("information extraction error: " + str(e))
+        print("Article Number extraction error: " + str(e))
         info.append("na")
 
     try:
         product_description = s.find("div", {"id": "salesArg"}).contents[0].strip()
         info.append(product_description)
     except Exception as e:
-        print("information extraction error: " + str(e))
+        print("Product Description extraction error: " + str(e))
         info.append("na")
 
     try:
         designer = s.find("div", {"id": "designer_right"}).contents[0].strip()
         info.append(designer)
     except Exception as e:
-        print("information extraction error: " + str(e))
+        print("Designer extraction error: " + str(e))
         info.append("na")
 
     try:
         num_of_ratings = s.find_all("a", {"class": "ratingsCount"})[1].contents[0].lstrip('(').rstrip(')')
         info.append(num_of_ratings)
     except Exception as e:
-        print("information extraction error: " + str(e))
+        print("Number of Ratings extraction error: " + str(e))
         info.append("na")
 
     try:
         overall_rating = s.find_all("a", {"class": "ratingsCount"})[0].contents[0]
         info.append(overall_rating)
     except Exception as e:
-        print("information extraction error: " + str(e))
+        print("Overall Rating extraction error: " + str(e))
         info.append("na")
 
     # product_dept = soup.find("a", {"id": "lnkMainMenu8Header"}).span.contents[0]
@@ -75,7 +77,7 @@ def get_prod_info(s):
         product_dept = s.find("a", {"id": "lnkMainMenu8Header"}).span.contents[0]
         info.append(product_dept)
     except Exception as e:
-        print("information extraction error: " + str(e))
+        print("Product Department extraction error: " + str(e))
         info.append("na")
 
     # product_subfunction = soup.find("meta", {"name": "category_name"})['content']
@@ -83,22 +85,26 @@ def get_prod_info(s):
         product_subfunction = s.find("meta", {"name": "category_name"})['content']
         info.append(product_subfunction)
     except Exception as e:
-        print("information extraction error: " + str(e))
+        print("Product Subfunction extraction error: " + str(e))
         info.append("na")
 
-    js = s.find_all("script", {"type": "text/javascript", "language":"JavaScript"})[9].string
-    js_2 = [s for s in js.splitlines() if "var jProductData" in s][0].strip().lstrip("var jProductData = ")
-    start_product_data = "pkgInfo\":"
-    temp = (js_2[js_2.index(start_product_data) + len(start_product_data):])
-    temp_list = eval(temp[:temp.index("}]")] + "}]")
-    # print (temp_list)
+    temp_list = [{'weightMet': 'na', 'widthMet': 'na', 'quantity': 'na', 'lengthMet': 'na', 'heightMet': 'na'}]
+    try:
+        js = s.find_all("script", {"type": "text/javascript", "language":"JavaScript"})
+        for j in js:
+            for x in j.contents:
+                if "var jProductData" in str(x):
+                    j_product_data = x[x.index("pkgInfo\":")+len("pkgInfo\":"):x.index("}]}]")] + "}]"
+                    temp_list = eval(j_product_data)
+    except Exception as e:
+        print("Product Data extraction error: " + str(e))
 
     # num_of_packages = temp_list[0]['quantity']
     try:
         num_of_packages = temp_list[0]['quantity']
         info.append(num_of_packages)
     except Exception as e:
-        print("information extraction error: " + str(e))
+        print("Quantity extraction error: " + str(e))
         info.append("na")
 
     # package_width = temp_list[0]['widthMet']
@@ -106,7 +112,7 @@ def get_prod_info(s):
         package_width = temp_list[0]['widthMet']
         info.append(package_width)
     except Exception as e:
-        print("information extraction error: " + str(e))
+        print("Package Width extraction error: " + str(e))
         info.append("na")
 
     # package_height = temp_list[0]['heightMet']
@@ -114,7 +120,7 @@ def get_prod_info(s):
         package_height = temp_list[0]['heightMet']
         info.append(package_height)
     except Exception as e:
-        print("information extraction error: " + str(e))
+        print("Package Height extraction error: " + str(e))
         info.append("na")
 
     # package_length = temp_list[0]['lengthMet']
@@ -122,7 +128,7 @@ def get_prod_info(s):
         package_length = temp_list[0]['lengthMet']
         info.append(package_length)
     except Exception as e:
-        print("information extraction error: " + str(e))
+        print("Package Length extraction error: " + str(e))
         info.append("na")
 
     # package_weight = temp_list[0]['weightMet']
@@ -130,18 +136,31 @@ def get_prod_info(s):
         package_weight = temp_list[0]['weightMet']
         info.append(package_weight)
     except Exception as e:
-        print("information extraction error: " + str(e))
+        print("Package Weight extraction error: " + str(e))
         info.append("na")
 
+
     # pdf link (download to a folder)
-    url_base = "https://www.ikea.com"
-    url_product_manual = s.find("div", {"id": "attachmentList"}).find_all('a')[1]['href']
-    product_manual = url_base + url_product_manual
-    # product_manual_pdf = 'manuals/' + url_product_manual + '.pdf'
-    response = requests.get(product_manual)
-    with open('manuals/test.pdf', 'wb') as f:
-        f.write(response.content)
-    print ("product manual saved to " + 'test.pdf')
+    # TODO: FIX manual extraction
+    try:
+        url_base = "https://www.ikea.com"
+        url_product_manual = s.find("div", {"id": "attachmentList"}).find_all('a')[1]['href']
+        
+        url_pdf = url_product_manual[14:-4].replace("/", "")
+        product_manual = url_base + url_product_manual
+        response = requests.get(product_manual)
+        pdf_name = url_pdf + '.pdf'
+        with open('manuals/' + pdf_name, 'wb') as f:
+            f.write(response.content)
+        info.append(pdf_name)
+        print ("product manual saved to " + pdf_name)
+    except Exception as e:
+        print("Product Manual extraction error: " + str(e))
+        print("no manual saved")
+
+    print (info)
+    print("---------------- END ----------------")
+    print ()
 
     return info
 
@@ -176,29 +195,39 @@ if __name__ == '__main__':
     #     pickle.dump(res, fpd)
 
     """ Next, download all product pages as html """
-    all_ids = []
-    with open('./product_ids.pkl', 'rb') as fpl:
-        all_ids = pickle.load(fpl)
-
-    # # TODO: got 11085 products, is this right?
-    # print (len(all_ids))
-    # print (type(all_ids))
-    # print (type(all_ids[0]))
-
-    # test_prod = 'https://www.ikea.com/us/en/catalog/products/70246089'
-    # test_prod_ood = 'https://www.ikea.com/us/en/catalog/products/00301499'
-    # urllib.request.urlretrieve(test_prod.encode('utf-8').decode('cp1252'), "test_prod.html")
-    # urllib.request.urlretrieve(test_prod_ood.encode('utf-8').decode('cp1252'), "test_prod_ood.html")
-
-    all_prods = [["Product Name", "Article Number", "Product Description", "Designer", "Number of Ratings", "Overall Rating", "Product Department", "Product Subfunction", "Number of Packages", "Package Width", "Package Height", "Package Length", "Package Weight", "Product Manual"]]
-    with open('test_prod.html') as fp:
-        soup = BeautifulSoup(fp, features='lxml')
-        soup_res = get_prod_info(soup)
-        print (soup_res)
-
+    # all_ids = []
+    # with open('./product_ids.pkl', 'rb') as fpl:
+    #     all_ids = pickle.load(fpl)
+    #     # print (all_ids)
+    #
+    # # # TODO: got 11085 ids --> 8050 products (3035 not found), is this right?
+    # # print (len(all_ids))
+    # # print (type(all_ids))
+    # # print (type(all_ids[0]))
+    #
+    # # test_prod = 'https://www.ikea.com/us/en/catalog/products/70246089'
+    # # test_prod_2 = 'https://www.ikea.com/us/en/catalog/products/30081467'
+    # # test_prod_2 = 'https://www.ikea.com/us/en/catalog/products/20275814'
+    # # test_prod_ood = 'https://www.ikea.com/us/en/catalog/products/00301499'
+    #
     # counter = 1
     # for prod_id in all_ids:
     #     prod_url = "https://www.ikea.com/us/en/catalog/products/" + prod_id
-    #     print (prod_url.encode('utf-8').decode('cp1252'))
-    #     # urllib.request.urlretrieve(ikea_url.encode('utf-8').decode('cp1252'), "depts/d" + str(counter) + ".html")
-    #     counter = counter + 1
+    #     try:
+    #         urllib.request.urlretrieve(prod_url.encode('utf-8').decode('cp1252'), "prods/p" + str(counter) + ".html")
+    #         counter = counter + 1
+    #     except Exception as e:
+    #         print("product " + prod_url + " not found: " + str(e))
+    #         print("SKIPPED")
+
+    """ Finally, download product information to csv """
+    # NB: can have multiple products for the same manual
+    all_prods = [["Product Name", "Article Number", "Product Description", "Designer", "Number of Ratings", "Overall Rating", "Product Department", "Product Subfunction", "Number of Packages", "Package Width", "Package Height", "Package Length", "Package Weight", "Product Manual"]]
+    for i in range(1, 8051):
+        product_file = 'prods/p' + str(i) + '.html'
+        print ()
+        print (product_file)
+        with open(product_file) as fp:
+            soup = BeautifulSoup(fp, features='lxml')
+            soup_res = get_prod_info(soup)
+            # TODO: write results to CSV
